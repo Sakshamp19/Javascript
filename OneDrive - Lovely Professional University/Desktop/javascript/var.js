0892fdde19be9d5d9bae526cbba43ef7643edb5b
Sakshamp19 <sakshamp428@gmail.com>
2025-05-06 19:01:22 +0530

    Initial commit

---
+++ OneDrive - Lovely Professional University/Desktop/javascript/var.js
@@ -0,0 +1,18 @@
+let accountId = 122534
+let accountNum=4545454545
+//dont use var keyword because of scope problems
+console.table([accountId,accountNum]);
+
+
+
+// number=>2 to power 53
+//bigint
+//string
+//bool
+// null->standalone value;
+//undefined;
+//symbol=>unique;
+//object
+console.log(typeof "saksham");
+console.log(typeof null); //null is treated as an object type;
+console.log(typeof undefined); //this is undefined type;
